df36b2e81eb06f1e71fb78aab1b82afd5bc63808
luckyiiiv <138688235+LuckyIIIV@users.noreply.github.com>
2026-02-01 19:40:49 +0100

    Update wl.lua

--- wl.lua
+++ wl.lua
@@ -1,5 +1,5 @@
 return {
-  "Lord_L4N", 3456463805, "d78c1499-b30b-4634-aa28-f5601a5898b3",
+  "Lord_L4N", 3456463805, "664e5cbc-730e-49ea-ac47-40da570fb3a2",
   "Lord_GrinderL", 4607756911, "a5c4dd56-8d6e-4026-864c-b83fb739a3e6",
   "iLikeLuckyCookies", 3214752215, "a5c4dd56-8d6e-4026-864c-b83fb739a3e6",
   "twistedgamer519", 1247779879, "9a00be6f-3a83-40b9-b8aa-de32adb7f117",
