829388f6f65042e98c5e7b663e3421ec887ad2a3
luckyiiiv <138688235+LuckyIIIV@users.noreply.github.com>
2026-08-22 23:11:23 +0200

    Remove 'XxFIREx1516' from wl.lua
    Removed an entry for 'XxFIREx1516' from the list.

--- wl.lua
+++ wl.lua
@@ -1,5 +1,4 @@
 return {
   "Lord_L4N", 3456463805, "664e5cbc-730e-49ea-ac47-40da570fb3a2",
-  "Lord_GrinderL", 4607756911, "a5c4dd56-8d6e-4026-864c-b83fb739a3e6",
-  "XxFIREx1516", 3252013546, "3bb71e90-4881-4dc1-b2de-f38242b1c294"
+  "Lord_GrinderL", 4607756911, "a5c4dd56-8d6e-4026-864c-b83fb739a3e6"
 }
